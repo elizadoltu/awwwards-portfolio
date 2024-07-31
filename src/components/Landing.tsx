aa1eea46547e92cf0b1ba21dfb919d8d14cdb711
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-07-31 17:29:58 +0300

    changes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import landingImage from "../assets/png/landing-image.png";
 import secondImage from "../assets/png/second-landing.png";
 import svgLanding from "../assets/png/svg-landing.png";
