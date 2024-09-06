f7f64763a68d075382fc2f6c1edf9ec4784f4ea4
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-09-06 16:49:43 +0300

    final changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ssr()],
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
